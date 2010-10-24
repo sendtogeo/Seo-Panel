1c79300ec0de5e5c056e3ca939b0dff69b903644
sendtogeo <sendtogeo@f92321fc-3d6b-43c1-41fb-d95dce3cdd13>
2010-10-24 17:22:10 +0000

    Seo Panel 2.2.0 changes

--- config/sp-config-sample.php
+++ config/sp-config-sample.php
@@ -40,7 +40,7 @@ define('DB_HOST', 'localhost');
 define('DB_ENGINE', 'mysql');
 
 # The version of seo panel installed
-define('SP_INSTALLED', '2.1.1');
+define('SP_INSTALLED', '2.2.0');
 
 # The DB debug mode
 define('SP_DEBUG', 0);
