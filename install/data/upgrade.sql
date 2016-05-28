b1b27c6f4dc067c4cb3858a2c8c5db42a414732e
sendtogeo <sendtogeo@gmail.com>
2016-05-28 15:42:56 +0530

    fix issue #13

--- install/data/upgrade.sql
+++ install/data/upgrade.sql
@@ -2,4 +2,7 @@
 -- Seo Panel 3.9.0 changes
 --
 
+INSERT INTO `settings` (`set_label`, `set_name`, `set_val`, `set_category`, `set_type`, `display`)  
+VALUES ('Seo Panel version', 'SP_VERSION_NUMBER', '3.9.0', 'system', 'medium', '0');
+
 UPDATE `searchengines` SET `regex` = '<div.*?class="?g.*?><h3 class="r"><a href="\\/url\\?q=(.*?)&amp;sa=U.*?>(.*?)<\\/a>.*?<\\/div><span.*?>(.*?)<\\/span>' WHERE `url` LIKE '%google%';
