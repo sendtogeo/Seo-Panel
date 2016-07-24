e287760fc4943cf11b7723c23b52c3af6035467a
CreaoticX <fluxkills@yahoo.com>
2016-07-23 22:12:24 -0700

    Altered the Bar Menu
    Put the bar menu into a for loop and added a filter so that menus could be added or removed.

--- themes/classic/views/menu/usermenu.ctp.php
+++ themes/classic/views/menu/usermenu.ctp.php
@@ -1,39 +1,33 @@
 <?php
-$homeClass = "";
-$adminClass = "";
-$seoToolsClass = "";
-$seoPluginsClass = "";
-switch($this->menu){
-	
-	case "support":
-		$supportClass = "current";
-		break;
-		
-	case "adminpanel":
-		$adminClass = "current";
-		break;		
-		
-	case "seotools":
-		$seoToolsClass = "current";
-		break;
-		
-	case "seoplugins":
-		$seoPluginsClass = "current";
-		break;
-		
-	case "home":
-	default:
-		$homeClass = "current";
-		break;
-} 
+
+$bar_menu = apply_filters("filter_bar_menu",array(
+    "home" => array("link" => SP_WEBPATH."/", "text" => $spText['common']['Dashboard']),
+    "seotools" => array("link" => SP_WEBPATH."/seo-tools.php", "text" => $spText['common']['Seo Tools']),
+    "seoplugins" => array("link" => SP_WEBPATH."/seo-plugins.php?sec=show", "text" => $spText['common']['Seo Plugins']),
+    "support" => array("link" => SP_WEBPATH."/support.php", "text" => $spText['common']['Support']),
+    "donate" => array("link" => SP_DONATE_LINK, "text" => $spText['common']['Donate']),
+    )
+        );
+foreach($bar_menu as $k => $v){
+    $add_class = "";
+    if($this->menu == $k){
+        $add_class = "current";
+    }
+    $target = "";
+    if($k == 'donate'){
+        $target = 'target="_blank"';
+    }
+    ?>
+<li><a class="<?php echo $add_class?>" href="<?php echo $v['link']?>" <?php echo $target?>><?php echo $v['text'] ?></a></li>
+<?php
+}
 ?>
-<li><a class="<?php echo $homeClass?>" href="<?php echo SP_WEBPATH?>/"><?php echo $spText['common']['Dashboard']?></a></li>
-<li><a class="<?php echo $seoToolsClass?>" href="<?php echo SP_WEBPATH?>/seo-tools.php"><?php echo $spText['common']['Seo Tools']?></a></li>
-<li><a class="<?php echo $seoPluginsClass?>" href="<?php echo SP_WEBPATH?>/seo-plugins.php?sec=show"><?php echo $spText['common']['Seo Plugins']?></a></li>
-<li><a class="<?php echo $supportClass?>" href="<?php echo SP_WEBPATH?>/support.php"><?php echo $spText['common']['Support']?></a></li>
-<li><a href="<?php echo SP_DONATE_LINK?>" target="_blank"><?php echo $spText['common']['Donate']?></a></li>
 <?php if (SP_DEMO) {?>
 	<li><a href="<?php echo SP_DOWNLOAD_LINK?>" target="_blank"><?php echo $spText['label']['Download']?></a></li>
 <?php }?>
-
+<?php 
+$adminClass = '';
+if($this->menu == 'adminpanel'){
+    $adminClass = "current";
+} ?>
 <li style="float: right; margin-right: 12px;"><a class="<?php echo $adminClass?>" href="<?php echo SP_WEBPATH?>/admin-panel.php"><?php echo $spText['common']['User Panel']?></a></li>
