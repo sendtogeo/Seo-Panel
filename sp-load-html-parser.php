8dc13b540dead0e7d24806d3bd45039749643237
CreaoticX <fluxkills@yahoo.com>
2016-07-24 22:37:59 -0700

    Added PHP HTML Parser
    Adding a more robut HTML parser to allow for greater analysis of crawled pages.

---
+++ sp-load-html-parser.php
@@ -0,0 +1,62 @@
+<?php
+
+
+//function __autoload($class_name) {
+//    require_once("./$class_name.php");
+//}
+
+//use stringEncode\Encode;
+//use stringEncode\Exception;
+//use PHPHtmlParser\Content;
+//use PHPHtmlParser\Curl;
+//use PHPHtmlParser\CurlInterface;
+//use PHPHtmlParser\Dom;
+//use PHPHtmlParser\Options;
+//use PHPHtmlParser\Selector;
+//use PHPHtmlParser\StaticDom;
+//use PHPHtmlParser\Dom\AbstractNode;
+//use PHPHtmlParser\Dom\ArrayNode;
+//use PHPHtmlParser\Dom\Collection;
+//use PHPHtmlParser\Dom\HtmlNode;
+//use PHPHtmlParser\Dom\InnerNode;
+//use PHPHtmlParser\Dom\LeafNode;
+//use PHPHtmlParser\Dom\MockNode;
+//use PHPHtmlParser\Dom\Tag;
+//use PHPHtmlParser\Dom\TextNode;
+//use PHPHtmlParser\Exceptions\ChildNotFoundException;
+//use PHPHtmlParser\Exceptions\CircularException;
+//use PHPHtmlParser\Exceptions\CurlException;
+//use PHPHtmlParser\Exceptions\EmptyCollectionException;
+//use PHPHtmlParser\Exceptions\NotLoadedException;
+//use PHPHtmlParser\Exceptions\ParentNotFoundException;
+//use PHPHtmlParser\Exceptions\StrictException;
+//use PHPHtmlParser\Exceptions\UnknownChildTypeException;
+
+require_once('stringEncode/Encode.php');
+require_once('stringEncode/Exception.php');
+require_once('PHPHtmlParser/Content.php');
+require_once('PHPHtmlParser/CurlInterface.php');
+require_once('PHPHtmlParser/Curl.php');
+require_once('PHPHtmlParser/Dom.php');
+require_once('PHPHtmlParser/Options.php');
+require_once('PHPHtmlParser/Selector.php');
+require_once('PHPHtmlParser/StaticDom.php');
+require_once('PHPHtmlParser/Dom/AbstractNode.php');
+require_once('PHPHtmlParser/Dom/ArrayNode.php');
+require_once('PHPHtmlParser/Dom/Collection.php');
+require_once('PHPHtmlParser/Dom/InnerNode.php');
+require_once('PHPHtmlParser/Dom/HtmlNode.php');
+require_once('PHPHtmlParser/Dom/LeafNode.php');
+require_once('PHPHtmlParser/Dom/MockNode.php');
+require_once('PHPHtmlParser/Dom/Tag.php');
+require_once('PHPHtmlParser/Dom/TextNode.php');
+require_once('PHPHtmlParser/Exceptions/ChildNotFoundException.php');
+require_once('PHPHtmlParser/Exceptions/CircularException.php');
+require_once('PHPHtmlParser/Exceptions/CurlException.php');
+require_once('PHPHtmlParser/Exceptions/EmptyCollectionException.php');
+require_once('PHPHtmlParser/Exceptions/NotLoadedException.php');
+require_once('PHPHtmlParser/Exceptions/ParentNotFoundException.php');
+require_once('PHPHtmlParser/Exceptions/StrictException.php');
+require_once('PHPHtmlParser/Exceptions/UnknownChildTypeException.php');
+
+?>
