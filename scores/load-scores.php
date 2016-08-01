2ecab40967e85c8d042d6e837530631df40d7bbe
CreaoticX <fluxkills@yahoo.com>
2016-08-01 16:47:05 -0700

    Fixed Score Broken Link File name

--- scores/load-scores.php
+++ scores/load-scores.php
@@ -4,7 +4,7 @@ include_once SP_LIBPATH.'/score.class.php';
 include_once SP_SCORESPATH.'/score-title.php';
 include_once SP_SCORESPATH.'/score-description.php';
 include_once SP_SCORESPATH.'/score-keywords.php';
-include_once SP_SCORESPATH.'/score-broke-link.php';
+include_once SP_SCORESPATH.'/score-broken-link.php';
 include_once SP_SCORESPATH.'/score-total-links.php';
 include_once SP_SCORESPATH.'/score-pagerank.php';
 include_once SP_SCORESPATH.'/score-backlinks-google.php';
