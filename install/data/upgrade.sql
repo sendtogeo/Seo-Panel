624ea89033e976919f3c3ddfdc79806e7145a755
Geo Varghese <gvarghese@aqorn.com>
2016-11-23 15:54:17 +0530

    to fix google search engine parse issue

--- install/data/upgrade.sql
+++ install/data/upgrade.sql
@@ -4,6 +4,8 @@
 
 update `settings` set set_val='3.10.0' WHERE `set_name` LIKE 'SP_VERSION_NUMBER';
 
+UPDATE `searchengines` SET `regex` = '<div.*?class="?g.*?><h3 class="r"><a.*?href="\\/url\\?q=(.*?)&amp;sa=U.*?>(.*?)<\\/a>.*?<\\/div><span.*?>(.*?)<\\/span>' WHERE `url` LIKE '%google%';
+
 
 
 
