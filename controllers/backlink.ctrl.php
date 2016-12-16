271997a4a7315bc5acc6732bb602abdfb07476ec
Geo Varghese <sendtogeo@gmail.com>
2016-12-16 15:23:31 +0530

    seo panel 3.10.0 : changes to fix bing back link and indexed link count

--- controllers/backlink.ctrl.php
+++ controllers/backlink.ctrl.php
@@ -27,7 +27,7 @@ class BacklinkController extends Controller{
 	var $backUrlList = array(
 		'google' => 'http://www.google.com/search?hl=en&q=link%3A',
 		'alexa' => 'http://www.alexa.com/siteinfo/',
-		'msn' => 'http://www.bing.com/search?setmkt=en-us&q=link%3A',
+		'msn' => 'http://www.bing.com/search?q=link%3A',
 	);
 	
 	function showBacklink() {
@@ -96,7 +96,6 @@ class BacklinkController extends Controller{
 			case 'msn':
 			    $url = formatUrl($this->url, false);
 				$url = $this->backUrlList[$engine] . urlencode(addHttpToUrl($url));
-				$this->spider->_CURLOPT_USERAGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36";
 				$v = $this->spider->getContent($url);
 				$pageContent = empty($v['page']) ? '' :  $v['page'];
 				
