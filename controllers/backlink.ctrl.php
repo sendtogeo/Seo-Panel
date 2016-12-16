9b04192e87f27b1a410cdf576b1d72d1773d57aa
Geo Varghese <sendtogeo@gmail.com>
2016-12-16 14:43:12 +0530

    seo panel 3.10.0 - Fix for bing backlink check

--- controllers/backlink.ctrl.php
+++ controllers/backlink.ctrl.php
@@ -96,8 +96,10 @@ class BacklinkController extends Controller{
 			case 'msn':
 			    $url = formatUrl($this->url, false);
 				$url = $this->backUrlList[$engine] . urlencode(addHttpToUrl($url));
+				$this->spider->_CURLOPT_USERAGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36";
 				$v = $this->spider->getContent($url);
 				$pageContent = empty($v['page']) ? '' :  $v['page'];
+				
 		        if (preg_match('/([0-9\,]+) results/si', $pageContent, $r)) {
 				} elseif (preg_match('/id="count".*?>.*?\(([0-9\,]+).*?\)/si', $pageContent, $r)) {
 				} elseif (preg_match('/id="count".*?>.*?([0-9\,]+).*?/si', $pageContent, $r)) {
