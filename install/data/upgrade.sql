fd96780cf787d8ac8fc0fc7dc6a9c06be7246684
Geo <sendtogeo@gmail.com>
2017-05-01 02:17:21 +0530

    seo panel 3.11.0 - #77 - to replace timestamp to date

--- install/data/upgrade.sql
+++ install/data/upgrade.sql
@@ -7,3 +7,7 @@ update `settings` set set_val='3.11.0' WHERE `set_name` LIKE 'SP_VERSION_NUMBER'
 ALTER TABLE `keywords` ADD `crawled` TINYINT( 1 ) NOT NULL DEFAULT '0';
 
 ALTER TABLE `websites` ADD `crawled` TINYINT( 1 ) NOT NULL DEFAULT '0';
+
+
+ALTER TABLE `rankresults` ADD `result_date` DATE NULL , ADD INDEX ( `result_date` );
+UPDATE `rankresults` SET `result_date` = FROM_UNIXTIME(result_time, '%Y-%m-%d');
