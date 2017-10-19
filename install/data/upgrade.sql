627f2aa206aec5382bb84342556f5125ebdfba38
Geo <sendtogeo@gmail.com>
2017-10-20 01:13:25 +0530

    seo panel 3.12.0 changes

--- install/data/upgrade.sql
+++ install/data/upgrade.sql
@@ -5,4 +5,13 @@
 update `settings` set set_val='3.12.0' WHERE `set_name` LIKE 'SP_VERSION_NUMBER';
 
 
+ALTER TABLE `crawl_log` CHANGE `subject` `subject` VARCHAR(60) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL;
+
+ALTER TABLE `auditorreports` CHANGE `page_title` `page_title` VARCHAR(180) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL;
+
+ALTER TABLE `auditorreports` CHANGE `page_description` `page_description` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL;
+
+ALTER TABLE `auditorreports` CHANGE `page_keywords` `page_keywords` TEXT CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL;
+
+ALTER TABLE `auditorreports` CHANGE `page_authority` `page_authority` FLOAT NOT NULL DEFAULT '0';
 
