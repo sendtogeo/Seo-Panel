5c50c0b8b90bbad3a6cb9fe1b5e0651610fe4a91
Geo Varghese <sendtogeo@gmail.com>
2018-09-15 02:15:23 +0530

    3.15.0 - changes for priority edit in seo plugins

--- themes/classic/views/seotools/listseotools.ctp.php
+++ themes/classic/views/seotools/listseotools.ctp.php
@@ -1,27 +1,28 @@
 <?php echo showSectionHead($spTextPanel['Seo Tools Manager']); ?>
-<?php echo $pagingDiv?>
-<table width="100%" border="0" cellspacing="0" cellpadding="0" class="list">
-	<tr class="listHead">
-		<td class="left"><?php echo $spText['common']['Id']?></td>
-		<td><?php echo $spText['common']['Name']?></td>
-		<td><?php echo $spTextTools['User Access']?></td>
-		<td><?php echo $spText['common']['Reports']?></td>
-		<td><?php echo $spText['label']['Cron']?></td>
-		<td class="right"><?php echo $spText['common']['Status']?></td>
-	</tr>
-	<?php
-	$colCount = 6; 
-	if(count($list) > 0){
-		$catCount = count($list);
+
+<script type="text/javascript">
+$(document).ready(function() { 
+    $("table").tablesorter({ 
+		sortList: [[2,0]]
+    });
+});
+</script>
+
+<table id="cust_tab" class="tablesorter">
+	<thead>
+		<tr class="listHead">
+			<th><?php echo $spText['common']['Id']?></th>
+			<th><?php echo $spText['common']['Name']?></th>
+			<th><?php echo $spText['common']['Priority']?></th>
+			<th><?php echo $spTextTools['User Access']?></th>
+			<th><?php echo $spText['common']['Reports']?></th>
+			<th><?php echo $spText['label']['Cron']?></th>
+			<th><?php echo $spText['common']['Status']?></th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
 		foreach($list as $i => $listInfo){
-			$class = ($i % 2) ? "blue_row" : "white_row";
-            if($catCount == ($i + 1)){
-                $leftBotClass = "tab_left_bot";
-                $rightBotClass = "tab_right_bot";
-            }else{
-                $leftBotClass = "td_left_border td_br_right";
-                $rightBotClass = "td_br_right";
-            }
 		
 			if($listInfo['status']){
 				$statLabel = $spText['common']["Active"];
@@ -40,22 +41,17 @@
             $accessLink = SP_DEMO ? scriptAJAXLinkHref('demo', '', "", $accessLabel) : scriptAJAXLinkHref('seo-tools-manager.php', 'content', "sec=changeaccess&seotool_id={$listInfo['id']}&user_access={$listInfo['user_access']}", $accessLabel);
             
 			?>
-			<tr class="<?php echo $class?>">
-				<td class="<?php echo $leftBotClass?>"><?php echo $listInfo['id'];?></td>
-				<td class="td_br_right left"><?php echo $listInfo['name'];?></td>
-				<td class="td_br_right"><?php echo $accessLink;?></td>				
-				<td class="td_br_right"><?php echo $reportgenLink;?></td>
-				<td class="td_br_right"><?php echo $cronLink;?></td>
-				<td class="<?php echo $rightBotClass?>" width="100px"><?php echo $activateLink;?></td>
+			<tr>
+				<td><?php echo $listInfo['id'];?></td>
+				<td><?php echo $listInfo['name'];?></td>
+				<td><?php echo $listInfo['priority'];?></td>
+				<td><?php echo $accessLink;?></td>				
+				<td><?php echo $reportgenLink;?></td>
+				<td><?php echo $cronLink;?></td>
+				<td><?php echo $activateLink;?></td>
 			</tr>
 			<?php
 		}
-	}else{	 
-		echo showNoRecordsList($colCount-2);		
-	} 
-	?>
-	<tr class="listBot">
-		<td class="left" colspan="<?php echo ($colCount-1)?>"></td>
-		<td class="right"></td>
-	</tr>
+		?>
+	</tbody>
 </table>
