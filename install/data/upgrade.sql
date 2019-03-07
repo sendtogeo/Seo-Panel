966d984f073a058794b6b6419b877bddf6cfa425
Geo Varghese <sendtogeo@gmail.com>
2019-03-08 02:35:54 +0530

    sp 3.18.0 - Test email settings feature

--- install/data/upgrade.sql
+++ install/data/upgrade.sql
@@ -5,3 +5,8 @@
 update `settings` set set_val='3.18.0' WHERE `set_name` LIKE 'SP_VERSION_NUMBER';
 
 
+INSERT INTO `texts` (`category`, `label`, `content`) VALUES 
+('settings', 'Send Email', 'Send Email'),
+('panel', 'Test Email Settings', 'Test Email Settings');
+
+
