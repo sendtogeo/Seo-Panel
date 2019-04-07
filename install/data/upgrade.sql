ea0423596605e8e2e6059ce353af2ea63097aa32
Geo Varghese <sendtogeo@gmail.com>
2019-04-07 14:56:00 +0530

    4.0.0 db changes

--- install/data/upgrade.sql
+++ install/data/upgrade.sql
@@ -4,9 +4,8 @@
 
 update `settings` set set_val='3.19.0' WHERE `set_name` LIKE 'SP_VERSION_NUMBER';
 
-INSERT INTO `texts` (`category`, `label`, `content`) 
-VALUES ('common', 'Plugins', 'Plugins'),
-VALUES ('common', 'Logout', 'Logout'),
-VALUES ('common', 'Tools', 'Tools');
 
-
+INSERT INTO `texts` (`category`, `label`, `content`) VALUES 
+('common', 'Plugins', 'Plugins'),                                                                                                                                                                    
+('common', 'Logout', 'Logout'),                                                                                                                                                                      
+('common', 'Tools', 'Tools');
