4d18693b5fb43c354402b1ef94eb86406b8c2174
sendtogeo <sendtogeo@gmail.com>
2019-05-12 01:01:11 +0530

    left menu

--- themes/classic/views/adminpanel/adminpanel.ctp.php
+++ themes/classic/views/adminpanel/adminpanel.ctp.php
@@ -1,7 +1,17 @@
-<div class="col-sm-2">
-	<?php include_once(SP_VIEWPATH."/adminpanel/adminleftmenu.ctp.php");?>
-</div>	
-<div class="col-sm-10">
+<div class="col-md-2" id="left_menu_sec">
+	<nav class="navbar navbar-expand-md navbar-light bg-light">
+      	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarLeftMenu"
+      		aria-controls="navbarLeftMenu" aria-expanded="false" aria-label="Toggle navigation">
+        	<span class="navbar-toggler-icon"></span>
+      	</button>      	
+    	<div class="collapse navbar-collapse" id="navbarLeftMenu">
+    		<ul class="navbar-nav mr-auto">
+				<?php include_once(SP_VIEWPATH."/adminpanel/adminleftmenu.ctp.php");?>
+    		</ul>
+    	</div>
+    </nav>
+</div>
+<div class="col-md-10">
     <div id="content">
 		<script type="text/javascript">
 			<?php echo $startFunction?>
