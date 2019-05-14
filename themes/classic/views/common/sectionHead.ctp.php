ec6927db8dd9269e90758de3545528a0ba9c2e61
Geo Varghese <sendtogeo@gmail.com>
2019-05-14 15:58:52 +0530

    4.0.0 table changes

--- themes/classic/views/common/sectionHead.ctp.php
+++ themes/classic/views/common/sectionHead.ctp.php
@@ -1,3 +1,3 @@
-<div class="SectionHeader" style="padding-bottom: 20px;">
-	<h1 style="border:0px;text-align:center;"><?php echo $sectionHead?></h1>
+<div class="SectionHeader">
+	<h3><?php echo $sectionHead?></h3>
 </div>
