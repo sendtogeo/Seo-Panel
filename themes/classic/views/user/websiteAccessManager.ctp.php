98dd57d883305b8bfb2758fbaa0d3bbf90213313
sendtogeo <sendtogeo@gmail.com>
2019-08-08 02:46:25 +0530

    cust dev changes

---
+++ themes/classic/views/user/websiteAccessManager.ctp.php
@@ -0,0 +1,10 @@
+<?php echo showSectionHead($spTextPanel['Website Access Manager']); ?>
+<form id="UserWebsiteAccess">
+<input type="hidden" name="sec" value="dowebsiteAccessManager"/>
+<table width="100%" cellpadding="0" class="list">
+	<tr class="listHead">
+		<td class="left" width='30%'><?php echo $spTextPanel['Website Access Manager']?></td>
+		<td class="right">&nbsp;</td>
+	</tr>
+</table>
+</form>
