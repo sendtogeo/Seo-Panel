fd01ad306edc493e310379ce3e79c6cb8b035faf
Geo Varghese <sendtogeo@gmail.com>
2019-09-10 19:29:07 +0530

    4.2.0 - adding time ago option to alert box

--- includes/sp-common.php
+++ includes/sp-common.php
@@ -757,4 +757,34 @@ function showExportDiv($pdfLink, $csvLink, $printLink) {
 	</div>
     <?php
 }
+
+function timeElapsedString($datetime, $full = false) {
+    $now = new DateTime;
+    $ago = new DateTime($datetime);
+    $diff = $now->diff($ago);
+    
+    $diff->w = floor($diff->d / 7);
+    $diff->d -= $diff->w * 7;
+    
+    $string = array(
+        'y' => 'year',
+        'm' => 'month',
+        'w' => 'week',
+        'd' => 'day',
+        'h' => 'hour',
+        'i' => 'minute',
+        's' => 'second',
+    );
+    
+    foreach ($string as $k => &$v) {
+        if ($diff->$k) {
+            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+    
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . ' ago' : 'just now';
+}
 ?>
