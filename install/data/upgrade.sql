eeefe51f10b00e24fb5f7a0ede5fb6b81c2ca597
sendtogeo <sendtogeo@gmail.com>
2019-12-16 01:26:51 +0530

    sp 4.3.0 - google search position fix

--- install/data/upgrade.sql
+++ install/data/upgrade.sql
@@ -4,3 +4,6 @@
 
 update `settings` set set_val='4.3.0' WHERE `set_name` LIKE 'SP_VERSION_NUMBER';
 
+
+UPDATE `searchengines` SET `regex` = '<div.*?class="?g.*?>.*?<div.*?class="r"*?>.*?<a href="(.*?)".*?>.*?<h3.*?>(.*?)<\\/h3>' WHERE `url` LIKE '%google%';
+
