3469b0f2a997f078b621dfdf396f458674bdd363
root <sendtogeo@gmail.com>
2020-07-04 01:33:34 +0530

    4.6.0 - Move unwanted config vars from config page to DB #185

--- install/data/upgrade.sql
+++ install/data/upgrade.sql
@@ -3,3 +3,8 @@
 --
 
 update `settings` set set_val='4.6.0' WHERE `set_name` LIKE 'SP_VERSION_NUMBER';
+
+INSERT INTO `settings` (`set_label`, `set_name`, `set_val`, `set_category`, `set_type`, `display`) VALUES
+('Seo Panel Demo', 'SP_DEMO', '0', 'system', 'large', 0),
+('Seo Panel Hosted Version', 'SP_HOSTED_VERSION', '0', 'system', 'large', 0),
+('Seo Panel Custom Dev', 'SP_CUSTOM_DEV', '0', 'system', 'large', 0);
