1c83cf821cf4125763754b33ef8dda1f479e889a
root <sendtogeo@gmail.com>
2020-07-04 01:06:16 +0530

    4.6.0 - base changes

--- install/data/upgrade.sql
+++ install/data/upgrade.sql
@@ -1,8 +1,5 @@
-
-
-
 --
--- Seo Panel 4.5.0 changes
+-- Seo Panel 4.6.0 changes
 --
 
-update `settings` set set_val='4.5.0' WHERE `set_name` LIKE 'SP_VERSION_NUMBER';
+update `settings` set set_val='4.6.0' WHERE `set_name` LIKE 'SP_VERSION_NUMBER';
