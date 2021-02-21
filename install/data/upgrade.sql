954b5887d1615b289b04509710030b8d52373b47
geo varghese <geo.varghese@c.thycotic.com>
2021-02-22 02:15:40 +0530

    sp 4.9.0 - moving regex to db for all engines

--- install/data/upgrade.sql
+++ install/data/upgrade.sql
@@ -7,5 +7,16 @@ update `settings` set set_val='4.9.0' WHERE `set_name` LIKE 'SP_VERSION_NUMBER';
 UPDATE `searchengines` SET `regex` = '<div.*?class=\"?g.*?>.*?<div.*?class=\"y.*?\">.*?<a href=\"(.*?)\".*?>.*?<h3.*?>(.*?)<\\/h3>' 
 WHERE `url` LIKE '%google%';
 
+CREATE TABLE `crawl_engines` (
+  `id` int UNSIGNED NOT NULL,
+  `engine_name` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `engine_category` varchar(60) COLLATE utf8_unicode_ci NOT NULL,
+  `regex1` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `regex2` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `regex3` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+ALTER TABLE `crawl_engines`  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `engine_name` (`engine_name`,`engine_category`);
+ALTER TABLE `crawl_engines` MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
 
