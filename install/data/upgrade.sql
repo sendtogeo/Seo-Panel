eba59909c75870baec964e9627683f943a765d63
Geo Varghese <sendtogeo@gmail.com>
2021-06-07 01:56:59 +0530

    4.10.0 quick proxy changes

--- install/data/upgrade.sql
+++ install/data/upgrade.sql
@@ -4,4 +4,9 @@
 
 update `settings` set set_val='4.10.0' WHERE `set_name` LIKE 'SP_VERSION_NUMBER';
 
+INSERT INTO `texts` (`category`, `label`, `content`) VALUES 
+('QuickWebProxy', 'Url blocked in the web proxy', 'Url blocked in the web proxy'),
+('QuickWebProxy', 'QWP_PROXY_BLOCK_URLS', 'Blocked Urls In Web Proxy'),
+('QuickWebProxy', 'QWP_PROXY_BLOCK_URLS_comment', 'Insert urls separated with comma to block in web proxy.');
+
 
