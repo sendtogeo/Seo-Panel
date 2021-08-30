56ca82c7752d156fd5ebd006effcc161cadaa2a9
geo varghese <geo.varghese@c.thycotic.com>
2021-08-31 01:47:29 +0530

    sp 4.10.0 release db changes

--- install/data/upgrade.sql
+++ install/data/upgrade.sql
@@ -4,7 +4,7 @@
 
 update `settings` set set_val='4.10.0' WHERE `set_name` LIKE 'SP_VERSION_NUMBER';
 update `featured_directories` set status=0 WHERE `directory_name` LIKE 'directorymaximizer.com';
-
+UPDATE `seoplugins` SET `version` = '2.1.0' WHERE name='QuickWebProxy';
 
 
 
